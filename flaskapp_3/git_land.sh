ab1686841b27d3b22541d66bdebee3197df2e6e0
flrnat001 <nathan.floor@gmail.com>
2013-03-06 12:02:26 +0200

    2013:03:06 12:02:08: Merge and land latest changes

---
+++ flaskapp_3/git_land.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Usage: git-land test_branch ...
+
+stamp=$(date)
+master=$(echo master)
+
+git add -vu
+git add -vA
+git commit -m "$stamp: Merge and land latest changes"
+git checkout $master
+git fetch --all -v --progress
+git pull origin $master
+git merge $1
+git push origin $master
+git checkout $1
+
+#set -x
+#git fetch --all
+#for branch in "$@"; do
+#    git checkout "$branch"      || exit 1
+#    git rebase "origin/$branch" || exit 1
+#done
