4b33d1cfb1b786e16f8a6661d7cf7edd5e2e6f4a
flrnat001 <nathan.floor@gmail.com>
2013-03-06 11:22:24 +0200

    up to db changes

--- flaskapp_3/app/__init__.py
+++ flaskapp_3/app/__init__.py
@@ -1,4 +1,16 @@
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views,models
+
+lm = LoginManager()
+lm.setup_app(app)
+oid = OpenID(app, os.path.join(basedir,'tmp'))
--- flaskapp_3/app/views.py
+++ flaskapp_3/app/views.py
@@ -1,5 +1,8 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
+from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +20,39 @@ def index():
     ]
     return render_template("index.html",title = 'Home', user = user, posts = posts)
 
+@app.route('/login',methods = ['GET','POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for = ['name','email'])
+    
+    return render_template('login.html',title = 'Sign In',form = form,providers = app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email id None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        name = resp.name
+        if name is None or name = "":
+            name = resp.email.split('@')[0]
+        user = User(name=name,email=resp.email,role=ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user,remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
